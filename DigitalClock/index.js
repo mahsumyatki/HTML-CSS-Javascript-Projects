f7005bca598604902a2e099794c327de1a2df244
MONSTER <MONSTER@DESKTOP-9EKD0KI>
2022-09-10 14:41:15 +0300

    Javascript code is added

---
+++ DigitalClock/index.js
@@ -0,0 +1,31 @@
+const hourElement = document.getElementById("hours");
+const minuteElement = document.getElementById("minutes");
+const secondElement = document.getElementById("seconds");
+const ampmElement = document.getElementById("ampm");
+
+function updateClock(){
+    let hour= new Date().getHours();
+    let minute= new Date().getMinutes();
+    let second= new Date().getSeconds();
+    let ampm= "AM";
+
+    if(hour>12){
+        hour -= 12;
+        ampm="PM";
+    }
+
+    hour=hour<10? "0" + hour:hour;
+    minute=minute<10? "0" + minute:minute;
+    second=second<10? "0" + second:second;
+
+    hourElement.innerText = hour;
+    minuteElement.innerText=minute;
+    secondElement.innerText=second;
+    ampmElement.innerText=ampm;
+
+    setTimeout(()=>{
+        updateClock();
+    },1000);
+}
+
+updateClock();
